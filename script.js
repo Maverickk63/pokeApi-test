398a888875eecd3d724bbb2c11d2815f25afa926
Maverickk63 <103067766+Maverickk63@users.noreply.github.com>
2024-02-05 10:32:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const url = 'https://pokeapi.co/api/v2/pokemon/pikachu'
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
